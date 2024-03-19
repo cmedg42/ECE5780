8cf4b6e0b2e4b6f9cd672e4e2556af85fe2a45a4
Carter Edginton <cmedginton@gmail.com>
2024-03-19 09:34:07 -0600

    Checkoff1

--- lab6/ece5780lab6/Core/Src/main.c
+++ lab6/ece5780lab6/Core/Src/main.c
@@ -18,43 +18,15 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
+void initleds(){
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+	GPIOC->MODER |= (1<<12 | 1<<14 | 1<<16 | 1<<18);
+	GPIOC->OTYPER &= ~(1<<6 | 1<<7 | 1<<8 | 1<<9);
+	GPIOC->OSPEEDR &= ~(1<<12 | 1<<14 | 1<<16 | 1<<18);
+	GPIOC->PUPDR &= ~(1<<12 | 1<<13 | 1<<14 | 1<<15 | 1<<16 | 1<<17 | 1<<18 | 1<<19);
+}
 
 /**
   * @brief  The application entry point.
@@ -62,40 +34,56 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	initleds();
+	
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;//enable gpioA
+	GPIOA->MODER |= GPIO_MODER_MODER1;//PA1 is now in analog mode
+	GPIOA->PUPDR &= ~(1<<3 | 1<<2); //PUPDR for PA1 is none
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;//enable ADC
+	
+	//configure ADC
+	ADC1->CFGR1 |= ADC_CFGR1_CONT | (1<<4); //continuous mode, 8 bit res part 1
+	ADC1->CFGR1 &= ~(1<<3);//8 bit res part 2
+	ADC1->CFGR1 &= ~(1<<11 | 1<<10);//HARDWARE trigger disabled
+	ADC1->CHSELR |= (1<<1); //ADC channel enable 1
+	
+	//CALIBRATION
+	if ((ADC1->CR & ADC_CR_ADEN) != 0) {//check ADEN = 0
+		ADC1->CR |= ADC_CR_ADDIS; //clear aden must be done by setting AD DiS
+	} 
+	while ((ADC1->CR & ADC_CR_ADEN) != 0) {}
+	ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN; //clear DMAEN
+	ADC1->CR |= ADC_CR_ADCAL; //start calibration
+	while ((ADC1->CR & ADC_CR_ADCAL) != 0)  {}//wait for adcal = 0
+	uint8_t factor = ADC1->DR & (1<<6|1<<5|1<<4|1<<3|1<<2|1<<1|1<<0);
+		
+		
+//ready
+		/* (1) Ensure that ADRDY = 0 */ /* (2) Clear ADRDY */ /* (3) Enable the ADC */ /* (4) Wait until ADC ready */ 
+	if ((ADC1->ISR & ADC_ISR_ADRDY) != 0) /* (1) */ {
+		ADC1->ISR |= ADC_ISR_ADRDY; /* (2) */ 
+	} 
+	ADC1->CR |= ADC_CR_ADEN; /* (3) */ 
+	while ((ADC1->ISR & ADC_ISR_ADRDY) == 0) /* (4) */ {
+  /* For robust implementation, add here time-out management */ 
+	}
+	
+	//start conversion
+	ADC1->CR |= (1<<2);//ADC conversion start
+	
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+		uint8_t read = ADC1->DR;
+		GPIOC->ODR &= ~(1<<6|1<<7|1<<8|1<<9);//turn all off RBOG
+		
+		//testing values
+		if (read > 10){GPIOC->ODR |= (1<<6);}
+		if (read > 50) {GPIOC->ODR |= (1<<7);}
+		if (read > 100){GPIOC->ODR |= (1<<8);}
+		if (read > 200){GPIOC->ODR |= (1<<9);}
   }
-  /* USER CODE END 3 */
 }
 
 /**
